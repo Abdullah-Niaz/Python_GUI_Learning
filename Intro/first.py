8576a7d09cb1db195be613236dd1113ddaecf020
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-07-12 03:48:42 +0500

    Tk Button Widgets

--- Intro/first.py
+++ Intro/first.py
@@ -2,6 +2,8 @@ from tkinter import *\
 
 #! Main window  object name 
 Box = Tk()
+frame = Frame(Box)
+frame.pack()
 
 #!  Giving the title to the window
 Box.title("First Program")
@@ -9,9 +11,10 @@ Box.title("First Program")
 
 #! Label is what output will be
 #! show on the window
-label = Label(Box, text="Hello World").pack()
+label = Label(frame, text="Hello World").pack(side=TOP)
+button = Button(frame, text="Hello", fg='blue',bg="green").pack()
 
 #! calling mainloop method which is used
 #! when your application is ready to run
 #! and it tells the code to keep displaying
-Box.mainloop()
+frame.mainloop()
