dbc08b91cff34ec9783ac194ceaa8d3e337d17ae
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-10-17 12:55:51 +0500

    widgets

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -1,8 +1,13 @@
 import streamlit as st
-
+import pandas as pd
 st.title("Stream Text Input")
- 
+
 name = st.text_input("Enter Your Name: ")
+age = st.slider("Select Your Age: ", 0, 100, 20)
+st.write("Your Age is ", age)
+options = ['python', 'c', 'c++', 'java', 'js']
+choice = st.selectbox("Choose your Favourite Language: ", options)
+st.write("You Select is : ", choice)
 if name:
     sst = name.strip()
     spt = name.split()
@@ -24,3 +29,21 @@ if name:
     st.write("Number of Total times: ", total)
     st.write(sst)
     st.write(spt)
+
+
+data = {
+    "Name": ["John", "Anna", "Peter", "Linda"],
+    "Age": [28, 24, 35, 32],
+    "Country": ["USA", "UK", "Australia", "Germany"]
+}
+
+df = pd.DataFrame(data)
+df.to_csv("SampleData.csv")
+
+st.write(df)
+
+
+uploaded_file = st.file_uploader("Choose CSV file", type='CSV')
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
